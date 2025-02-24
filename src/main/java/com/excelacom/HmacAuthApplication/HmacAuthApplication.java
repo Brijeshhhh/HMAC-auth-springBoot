eb6887927931617dec7339de3f2877c6c9fed832
Brijesh Babu <Brijesh.B@allfon.in>
2025-02-24 10:10:30 +0530

    Fix line endings

---
+++ src/main/java/com/excelacom/HmacAuthApplication/HmacAuthApplication.java
@@ -0,0 +1,14 @@
+package com.excelacom.HmacAuthApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class HmacAuthApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(HmacAuthApplication.class, args);
+	}
+}
+
+
